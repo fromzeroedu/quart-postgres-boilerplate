5934e8867b3bf16526be5fa44e5553326698a54c
From Zero <jorge@fromzero.io>
2022-01-27 10:28:38 -0500

    test 2

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -4,7 +4,6 @@ from quart import current_app
 from counter.models import counter_table
 
 
-@pytest.mark.asyncio
 async def test_initial_response(create_test_client):
     response = await create_test_client.get("/")
     body = await response.get_data()
