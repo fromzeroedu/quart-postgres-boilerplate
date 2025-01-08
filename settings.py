5728b0db6db6fab44d738eafcf904213bfcd4720
Jorge Escobar <j@jorge3.com>
2025-01-08 11:20:18 -0500

    updating to new verion

--- settings.py
+++
@@ -1,9 +0,0 @@
-import os
-
-QUART_APP = os.environ["QUART_APP"]
-QUART_ENV = os.environ["QUART_ENV"]
-SECRET_KEY = os.environ["SECRET_KEY"]
-DB_USERNAME = os.environ["DB_USERNAME"]
-DB_PASSWORD = os.environ["DB_PASSWORD"]
-DB_HOST = os.environ["DB_HOST"]
-DATABASE_NAME = os.environ["DATABASE_NAME"]
